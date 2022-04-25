578f67b6d47298cca132b4a630148b060fbf9ee4
Daniel Sukenik <sukenik9@gmail.com>
2022-04-25 16:34:12 +0300

    Deleted unnecessary MainPage

--- src/Pages/MainPage.tsx
+++
@@ -1,8 +0,0 @@
-import StatusChain from "../Components/StatusChain/statusChain";
-import { MainPageLayout } from "../Layouts/MainPageLayout/MainPageLayout";
-
-export const MainPage = () => {
-    return (
-        <></>
-    );
-};
