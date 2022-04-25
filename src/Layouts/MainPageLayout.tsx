05434139d642defd64edb82c4c0ce9167b66e854
Daniel Sukenik <sukenik9@gmail.com>
2022-04-25 11:10:12 +0300

    Created Main Page Layout

---
+++ src/Layouts/MainPageLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MainPageLayout: React.FC = () => {
+    return (
+        <>
+            <header>IDF site</header>
+            <img src="" alt="" />
+            <div id="status-chain"></div>
+        </>
+    );
+};
