74880bd50038ec9a44de8c7a7f3744276dd51765
Daniel Sukenik <sukenik9@gmail.com>
2022-04-25 19:05:35 +0300

    Fix Styles

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -35,12 +35,12 @@ const LandingPage = () => {
                     !tripMode ? 
                     <Grid container direction='column' justifyContent='center' alignContent='center' className={styles.landingPageContainer} style={{height: "100vh"}}>
                         <Grid item>
-                            <Button variant="contained" color="primary" onClick={setOutTripMode} className={classes.button} style={{margin: "5px"}}>
+                            <Button variant="contained" color="primary" onClick={setOutTripMode} className={classes.button}>
                                 יוצא
                             </Button>
                         </Grid>
                         <Grid item>
-                            <Button variant="contained" color="primary" onClick={setEnteyTripMode} className={classes.button} style={{margin: "5px"}}>
+                            <Button variant="contained" color="primary" onClick={setEnteyTripMode} className={classes.button}>
                                 נכנס
                             </Button>
                         </Grid>
